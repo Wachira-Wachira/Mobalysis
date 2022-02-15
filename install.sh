1f1beaa33cf6331c9de1e5bd361ea634f674fa9a
Juma-Sindani <james.sindani@azubiafrica.org>
2022-02-15 18:21:13 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,26 @@
-#! /bin/bash
-sudo su
+#!/bin/bash
 
-git clone https://github.com/PaulineNjokiMaina/Mobalysis.git ./PATHs
+
+sudo useradd  mob_app_usr -n -d /users/mob_app_usr
+sudo git clone https://github.com/PaulineNjokiMaina/Mobalysis.git ./users/mob_app_usr
+
+echo executing Env.variables
+export DBNAME=mobalytics
+export DBUSER=mob_db_user
+export DBPASS=mob_db_pass
+export DBHOST=localhost
+export DBPORT=54322
+
+sudo apt install python3.8-venv
+
+#Setting up the environment
+
+python3 -m venv env
+
+#Activating the envirionment
+source env/bin/activate
+
+
+#Install packages
+
+pip3 install django
