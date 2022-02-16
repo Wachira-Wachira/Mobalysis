a74268bddcc3a308446e4f03cc70deb06734d8c5
MKwanda <mbrkkwanda@gmail.com>
2022-02-16 06:25:14 +0000

    modified to include comments and  django migration

--- install.sh
+++ install.sh
@@ -1,26 +1,34 @@
-#!/bin/bash
+#! /bin/bash
 
-
+#ceating user and home directory
 sudo useradd  mob_app_usr -n -d /users/mob_app_usr
-sudo git clone https://github.com/PaulineNjokiMaina/Mobalysis.git ./users/mob_app_usr
+#cloning repo to users home directory
+sudo git clone https://github.com/PaulineNjokiMaina/Mobalysis.git .//mob_app_usr
 
-echo executing Env.variables
+#Creating Env. variables
+echo Creating Env.variables
 export DBNAME=mobalytics
 export DBUSER=mob_db_user
 export DBPASS=mob_db_pass
 export DBHOST=localhost
 export DBPORT=54322
 
+#Installing backend Env.Variables
 sudo apt install python3.8-venv
 
 #Setting up the environment
 
-python3 -m venv env
+sudo python3 -m venv env
 
 #Activating the envirionment
-source env/bin/activate
+sudo source env/bin/activate
 
 
 #Install packages
 
-pip3 install django
+sudo pip3 install django
+
+#Creating Migration
+
+sudo python3 /users/mob_app_usr/manage.py makemigrations 
+sudo python3 /users/mob_app_usr/manage.py migrate
