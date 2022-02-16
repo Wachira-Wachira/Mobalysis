f3c03a02236815193547882606db5ccb18bdd3e4
MKwanda <mbrkkwanda@gmail.com>
2022-02-16 12:42:38 +0000

    corrections to code Env paths

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
 #ceating user and home directory
-sudo useradd  mob_app_usr -n -d /users/mob_app_usr
+sudo useradd  mob_app_usr -m -d /home/mob_app_usr
 #cloning repo to users home directory
-sudo git clone https://github.com/PaulineNjokiMaina/Mobalysis.git ./users/mob_app_usr
+sudo git clone https://github.com/PaulineNjokiMaina/Mobalysis.git ./home/mob_app_usr
 
 #Creating Env. variables
 echo Creating Env.variables
-export DBNAME=mobalytics
-export DBUSER=mob_db_user
-export DBPASS=mob_db_pass
-export DBHOST=localhost
-export DBPORT=54322
+sudo echo "export DBNAME=mobalytics" >> home/mob_app_usr/.bashrc
+sudo echo "export DBUSER=mob_db_user" >> home/mob_app_usr/.bashrc
+sudo echo "export DBPASS=mob_db_pass" >> home/mob_app_usr/.bashrc
+sudo echo "export DBHOST=localhost" >> home/mob_app_usr/.bashrc
+sudo echo "export DBPORT=54322" >> home/mob_app_usr/.bashrc
 
 #Installing backend Env.Variables
 sudo apt install python3.8-venv
@@ -24,11 +24,14 @@ sudo python3 -m venv env
 sudo source env/bin/activate
 
 
-#Install packages
+#Install Django packages
+echo Installing Django 
 
-sudo pip3 install django
+sudo apt-get update
 
-#Creating Migrations
+sudo apt install python3-django
 
-sudo python3 /users/mob_app_usr/manage.py makemigrations 
-sudo python3 /users/mob_app_usr/manage.py migrate
+#Creating Migration
+echo Creating Migration 
+sudo python3 home/mob_app_usr/backend/manage.py makemigrations 
+sudo python3 home/mob_app_usr/backend/manage.py migrate
