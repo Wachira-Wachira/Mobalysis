9be362455e703e442feb88bd7677b6d71a0f077a
Juma-Sindani <james.sindani@azubiafrica.org>
2022-02-18 11:27:58 +0300

    Add command to install requirements

--- install.sh
+++ install.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
+#! /bin/bash
 
-sudo useradd  mob_app_usr -n -d /users/mob_app_usr
-sudo git clone https://github.com/PaulineNjokiMaina/Mobalysis.git ./users/mob_app_usr
+#ceating user and home directory
+sudo useradd  mob_app_usr -m -d /home/mob_app_usr
+#cloning repo to users home directory
+sudo git clone https://github.com/PaulineNjokiMaina/Mobalysis.git ./home/mob_app_usr
 
-echo executing Env.variables
-export DBNAME=mobalytics
-export DBUSER=mob_db_user
-export DBPASS=mob_db_pass
-export DBHOST=localhost
-export DBPORT=54322
+#Creating Env. variables
+echo Creating Env.variables
+sudo echo "export DBNAME=mobalytics" >> home/mob_app_usr/.bashrc
+sudo echo "export DBUSER=mob_db_user" >> home/mob_app_usr/.bashrc
+sudo echo "export DBPASS=mob_db_pass" >> home/mob_app_usr/.bashrc
+sudo echo "export DBHOST=localhost" >> home/mob_app_usr/.bashrc
+sudo echo "export DBPORT=54322" >> home/mob_app_usr/.bashrc
 
+#Installing backend Env.Variables
 sudo apt install python3.8-venv
 
 #Setting up the environment
 
-python3 -m venv env
+
+
+sudo python3 -m venv env
 
 #Activating the envirionment
-source env/bin/activate
+sudo source env/bin/activate
 
 
-#Install packages
+#Install Django packages
+echo Installing Django 
 
-pip3 install django
+
+
+sudo cd backend
+sudo pip3 install -r requirements.txt
+#Creating Migration
+echo Creating Migration 
+sudo python3 home/mob_app_usr/backend/manage.py makemigrations 
+sudo python3 home/mob_app_usr/backend/manage.py migrate
